50744231987baa60467cb1b002ec7e44c3368ba6
clarity-ai-test <alejandrogfer96@gmail.com>
2026-02-06 22:21:16 +0100

    chore: add detekt, ktlint and CI verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,39 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     application
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    jacoco
 }
 
 repositories {
     mavenCentral()
 }
 
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom(files("$rootDir/detekt.yml"))
+}
+
 dependencies {
-    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-application {
-    mainClass.set("app.ConsoleAppKt")
-}
-
 kotlin {
     jvmToolchain(21)
+}
+
+ktlint {
+    version.set("1.3.1")
+}
+
+tasks.check {
+    dependsOn("test", "detekt", "ktlintCheck")
 }
